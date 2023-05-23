2225aa617d937b373187afebc8be890c0acbca64
KRAUTH Mickael <mickaell.kr@gmail.com>
2023-05-23 13:48:32 +0200

    added css to modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef, useCallback } from 'react'
 import styles from './styles.module.css'
+const TestModal = ({ isOpen, children, onClose }) => {
+  const ref = useRef(null)
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  const clickListener = useCallback(
+    (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClose()
+      }
+    },
+    [onClose]
+  )
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', clickListener)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', clickListener)
+    }
+  }, [isOpen, clickListener])
+
+  if (!isOpen) return null
+
+  return (
+    <div className={styles['modal-overlay']}>
+      <div className={styles.modal} ref={ref}>
+        <button className={styles['modal-close']} onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  )
 }
+
+export default TestModal
